542acc3d1ff9cce5db9911974c4ee3463418e0af
kimys <ys2life1@naver.com>
2023-04-01 19:39:04 +0900

    리액트아이콘, 라우터돔, 스타일컴포넌, zustand 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  background-color: black;
+`;
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Div>dkssud</Div>
+    </div>
+  );
 }
 
 export default App;
