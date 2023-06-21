815c91b0a00a790b3cc8416a877ac0e8b0e60d26
kimys-kor <ys2life1@naver.com>
2023-06-21 22:46:07 +0900

    회원 상세창 탭 버튼 active효과

--- src/components/details/MemberDetail.tsx
+++ src/components/details/MemberDetail.tsx
@@ -51,6 +51,9 @@ function MemberDetail() {
   const navigate = useNavigate();
 
   const [tab, setTab] = useState<number>(1);
+  const handleTab = (tab: number) => {
+    setTab(tab);
+  };
 
   return (
     <main className="flex flex-col w-full h-full p-4 gap-4 bg-[#fff]">
@@ -69,156 +72,181 @@ function MemberDetail() {
 
       <div className="min-w-fit mt-4">
         <button
-          className="w-24 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0]
-          hover:border-active hover:text-active "
+          onClick={() => setTab(1)}
+          className={`w-24 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0] ${
+            tab === 1
+              ? "border-active text-active"
+              : "hover:border-active hover:text-active"
+          }`}
         >
           회원정보
         </button>
         <button
-          className="w-28 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0]  
-        hover:border-active hover:text-active  "
+          onClick={() => setTab(2)}
+          className={`w-28 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0] ${
+            tab === 2
+              ? "border-active text-active"
+              : "hover:border-active hover:text-active"
+          }`}
         >
           통합주문내역
         </button>
         <button
-          className="w-24 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0]  
-        hover:border-active hover:text-active "
+          onClick={() => setTab(3)}
+          className={`w-24 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0] ${
+            tab === 3
+              ? "border-active text-active"
+              : "hover:border-active hover:text-active"
+          }`}
         >
           적립금내역
         </button>
         <button
-          className="w-24 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0]  
-        hover:border-active hover:text-active "
+          onClick={() => setTab(4)}
+          className={`w-24 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0] ${
+            tab === 4
+              ? "border-active text-active"
+              : "hover:border-active hover:text-active"
+          }`}
         >
           예치금내역
         </button>
         <button
-          className="w-28 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0]  
-        hover:border-active hover:text-active "
+          className={`w-28 p-1 text-[1rem] shadow-[0px_1px_3px_0px_#dadce0] ${
+            tab === 5
+              ? "border-active text-active"
+              : "hover:border-active hover:text-active"
+          }`}
         >
           1:1문의내역
         </button>
       </div>
 
-      <section className="flex flex-col w-full gap-1 ">
-        <div className="flex w-full ">
-          <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
-            이름
-          </p>
-          <div className="flex items-center w-2/6 pl-1 border-b-2 min-w-30">
-            <input
-              className="w-16 h-4/5 border shadow-[0px_1px_1px_0px_#dadce0_inset]"
-              defaultValue={"안녕"}
-            ></input>
-          </div>
+      {tab === 1 && (
+        <>
+          <section className="flex flex-col w-full gap-1 ">
+            <div className="flex w-full ">
+              <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
+                이름
+              </p>
+              <div className="flex items-center w-2/6 pl-1 border-b-2 min-w-30">
+                <input
+                  className="w-16 h-4/5 border shadow-[0px_1px_1px_0px_#dadce0_inset]"
+                  defaultValue={"안녕"}
+                ></input>
+              </div>
 
-          <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
-            회원등급
-          </p>
-          <div className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
-            <select className="w-20 shadow-[0px_1px_1px_0px_#dadce0_inset]">
-              <option value="플래티넘">플래티넘</option>
-              <option value="골드">골드</option>
-              <option value="실버">실버</option>
-            </select>
-          </div>
-        </div>
+              <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
+                회원등급
+              </p>
+              <div className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
+                <select className="w-20 shadow-[0px_1px_1px_0px_#dadce0_inset]">
+                  <option value="플래티넘">플래티넘</option>
+                  <option value="골드">골드</option>
+                  <option value="실버">실버</option>
+                </select>
+              </div>
+            </div>
 
-        <div className="flex w-full">
-          <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
-            적립금
-          </p>
-          <div className="flex items-center w-2/6 gap-3 px-1 truncate border-b-2">
-            1,000
-            <form onSubmit={onSubmit} className="flex ">
-              <input
-                className="w-44 h-4/5 border shadow-[0px_1px_1px_0px_#dadce0_inset]"
-                placeholder="적립 금액을 입력해 주세요."
-                type="number"
-                {...register("savingPoint")}
-              ></input>
-              <button
-                className="w-16 h-4/5 bg-active text-[#fff]"
-                type="submit"
-                value="적립"
-              >
-                적립
-              </button>
-            </form>
-          </div>
+            <div className="flex w-full">
+              <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
+                적립금
+              </p>
+              <div className="flex items-center w-2/6 gap-3 px-1 truncate border-b-2">
+                1,000
+                <form onSubmit={onSubmit} className="flex ">
+                  <input
+                    className="w-44 h-4/5 border shadow-[0px_1px_1px_0px_#dadce0_inset]"
+                    placeholder="적립 금액을 입력해 주세요."
+                    type="number"
+                    {...register("savingPoint")}
+                  ></input>
+                  <button
+                    className="w-16 h-3/5 bg-active text-[#fff]"
+                    type="submit"
+                    value="적립"
+                  >
+                    적립
+                  </button>
+                </form>
+              </div>
 
-          <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
-            예치금
-          </p>
-          <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">1,000</p>
-        </div>
+              <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
+                예치금
+              </p>
+              <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">1,000</p>
+            </div>
 
-        <div className="flex w-full">
-          <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
-            누적총로그인
-          </p>
-          <div className="flex items-center w-2/6 pl-1 border-b-2">6</div>
+            <div className="flex w-full">
+              <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
+                누적총로그인
+              </p>
+              <div className="flex items-center w-2/6 pl-1 border-b-2">6</div>
 
-          <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
-            주문완료 상품/금액
-          </p>
-          <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">vip</p>
-        </div>
+              <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
+                주문완료 상품/금액
+              </p>
+              <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">vip</p>
+            </div>
 
-        <div className="flex w-full">
-          <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white ">
-            가입일자
-          </p>
-          <div className="flex items-center w-2/6 pl-1 border-b-2 ">
-            2023-06-20 17:13:00
-          </div>
+            <div className="flex w-full">
+              <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white ">
+                가입일자
+              </p>
+              <div className="flex items-center w-2/6 pl-1 border-b-2 ">
+                2023-06-20 17:13:00
+              </div>
 
-          <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white ">
-            최종방문일
-          </p>
-          <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
-            2023-06-20 17:13:00
-          </p>
-        </div>
+              <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white ">
+                최종방문일
+              </p>
+              <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
+                2023-06-20 17:13:00
+              </p>
+            </div>
 
-        <div className="flex w-full">
-          <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
-            비밀번호
-          </p>
-          <div className="flex items-center w-2/6 pl-1 border-b-2">
-            2023-06-20 17:13:00
-          </div>
+            <div className="flex w-full">
+              <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
+                비밀번호
+              </p>
+              <div className="flex items-center w-2/6 pl-1 border-b-2">
+                2023-06-20 17:13:00
+              </div>
 
-          <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
-            강제탈퇴
-          </p>
-          <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
-            2023-06-20 17:13:00
-          </p>
-        </div>
-        <div className="flex w-full">
-          <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
-            휴대폰
-          </p>
-          <div className="flex items-center w-2/6 pl-1 border-b-2">
-            010-1234-1234
-          </div>
+              <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
+                강제탈퇴
+              </p>
+              <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
+                2023-06-20 17:13:00
+              </p>
+            </div>
+            <div className="flex w-full">
+              <p className="w-1/6 min-w-fit pl-1  bg-[#EFEFEF] border-b-white">
+                휴대폰
+              </p>
+              <div className="flex items-center w-2/6 pl-1 border-b-2">
+                010-1234-1234
+              </div>
 
-          <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
-            이메일
-          </p>
-          <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">wkek@naver.com</p>
-        </div>
-      </section>
+              <p className="w-1/6 min-w-fit pl-1 min-w-10 bg-[#EFEFEF] border-b-white">
+                이메일
+              </p>
+              <p className="w-2/6 px-2 pl-1 border-b-2 min-w-fit">
+                wkek@naver.com
+              </p>
+            </div>
+          </section>
 
-      <section className="flex flex-col w-full gap-1 mt-4">
-        <div className="flex w-full h-12 items-center justify-center">
-          <p className="w-1/6 h-12 leading-10 min-w-fit pl-1  bg-[#EFEFEF] border-b-white ">
-            회원 메모
-          </p>
-          <textarea className="w-5/6 pl-2 border shadow-[0px_1px_1px_0px_#dadce0_inset] focus:outline-none focus:shadow-none resize-none"></textarea>
-        </div>
-      </section>
+          <section className="flex flex-col w-full gap-1 mt-4">
+            <div className="flex w-full h-12 items-center justify-center">
+              <p className="w-1/6 h-12 leading-10 min-w-fit pl-1  bg-[#EFEFEF] border-b-white ">
+                회원 메모
+              </p>
+              <textarea className="w-5/6 pl-2 border shadow-[0px_1px_1px_0px_#dadce0_inset] focus:outline-none focus:shadow-none resize-none"></textarea>
+            </div>
+          </section>
+        </>
+      )}
     </main>
   );
 }
